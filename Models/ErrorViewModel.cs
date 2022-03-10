0f663e2931ceb10adf5f6a37a6a4bc459d706f3e
unknown <kartiselmuthu@hotmail.com>
2022-03-10 13:41:16 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyFirstTesting.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
